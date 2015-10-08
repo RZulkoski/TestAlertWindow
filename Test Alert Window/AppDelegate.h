29ab260a1870e9e1c3237a340735ca269cc6880c
Ryan Zulkoski <ryan.zulkoski@teamsoftware.com>
2015-10-08 07:10:31 -0500

    Initial Commit

---
+++ Test Alert Window/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test Alert Window
+//
+//  Created by Ryan Zulkoski on 10/8/15.
+//  Copyright © 2015 Ryan Zulkoski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test Alert Window/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test Alert Window
+//
+//  Created by Ryan Zulkoski on 10/8/15.
+//  Copyright © 2015 Ryan Zulkoski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
